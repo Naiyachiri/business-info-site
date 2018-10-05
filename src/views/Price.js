c0d5853a72d8ba5fa7b024a0d60f1a7d3e94a35a
James Bui <buijvbio@gmail.com>
2018-10-05 13:28:43 -0400

    FEAT: added price list
    divided it responsively by column according to screen resolution

--- src/views/Price.js
+++ src/views/Price.js
@@ -4,7 +4,84 @@ export default class Price extends React.Component {
 
   render() {
     return(
-      <div>price list</div>
+      <div className='price-container'>
+        <h1>Hollywood Nails</h1>
+        <span> Premiere Nail Salon for Men and woman </span>
+        <div style={{width: '200px', height: '100px', backgroundColor: 'white', textAlign: 'center'}}> some kind of image</div>
+        <p>LOCATED IN PATRICK HENRY MALL</p>
+        <p>12300 Jefferson Ave, Unit 422</p>
+        <p>Newport News, VA 23602</p>
+        <p>(157) 249-4033</p>
+        <p>Price List</p>
+        
+        
+        <h5>Acrylic</h5>
+        <ul>
+          <li>Full Set $25 & up</li>
+          <li>Fill In $15 & up</li>
+        </ul>
+
+        <h5>Solar</h5>
+        <ul>
+          <li>Full Set Pink & White $50 & up</li>
+          <li>Fill In Pink & White $35 & up</li>
+        </ul>
+
+        <h5>Gel </h5>
+        <ul>
+          <li>Full Set $25 & up</li>
+          <li>Fill In $15 & up</li>
+        </ul>
+        <p>Manicure $14 & up</p>
+        <p>Pedicure $25 & up</p>
+        <p>Children's Pedicure (Under 10) $18 & up</p>
+        <p>Deluxe Spa Manicure $20 & up</p>
+        <p>Deluxe Spa Pedicure $35 & up</p>
+        <p>Spa Manicure & Pedicure $55 & up</p>
+        <p>NuSkin Spa Combo (Pedi/Mani) $20/$35 & up</p>
+        <p>Express Facial $25 & up</p>
+        <p>Overlay $25 & up</p>
+        <p>Nail Repair $3 & up</p>
+        <p>Cut Down $3 & up</p>
+        <p>Buffer Shine $5 $ up</p>
+
+        <h5>Gelish Color</h5>
+        <ul>
+          <li>Basic $20 & up</li>
+          <li>French/Color Tips $25 & up</li>
+        </ul>
+
+        <p>Gel/ Acrylic Removal $10</p>
+        <h5>Basic Polish Change</h5>
+        <ul>
+          <li>Hands $8</li>
+          <li>Feet $10</li>
+        </ul>
+        <p>Special Designs $5</p>
+
+        <h5>Waxing</h5>
+        <ul>
+          <li>Eyebrows $8</li>
+          <li>Chin $8</li>
+          <li>Temples $8</li>
+          <li>Full Face $35 & up</li>
+        </ul>
+
+        <h4> Color Tips/French/American Manicure 
+          <span> are an additional $5</span></h4>
+      </div>
     )
   }
 }
+
+// _iWi
+// Polish Change $Hands) $.00 8: U9
+// Polish Change {Toes} , L0.  ___& l-IQ
+// Design $5.00 $ ug
+// Waxing Eyebrows $8.00 & ug Lig $'/.00 & ug
+// Chin $8.00 & ug Cheeks $15.00 & ug
+// Temgles $8.00 & up Full Face $35.00 & ug
+// *French/American Manicure/Color Tips an
+// additional $5.00
+// APPOINTMENTS AND WALK-INS WELCOME
+// HOURS: Monday-Saturday 10am - 9pm. Sunday IZPH1 - 5P"1
